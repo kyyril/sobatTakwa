f8d0c164364bf7b9243b3e50e9b456e6496158aa
kyyril <khairilrahman55@gmail.com>
2024-09-17 05:52:17 +0700

    fetch: detail surah

---
+++ src/app/quran/[slug]/page.tsx
@@ -0,0 +1,36 @@
+import { UrlSurah } from "@/lib/utils/fetcher";
+async function fetchDetail(slug: string) {
+  try {
+    const response = await fetch(`${UrlSurah}/${slug}`, {
+      cache: "no-store",
+    });
+    if (!response.ok) {
+      throw new Error(`Failed to fetch detail: ${response.statusText}`);
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching detail:", error);
+    return null;
+  }
+}
+export default async function detailSurah({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const detailSurah = await fetchDetail(params.slug);
+  if (!detailSurah) {
+    return <p>Error loading details. Please try again later.</p>;
+  }
+  return (
+    <div className="w-full flex justify-center items-start min-h-screen mt-16">
+      <div key={detailSurah.number}>
+        <h1 className="m-2 text-center font-sans font-extrabold text-4xl">
+          {detailSurah.number}. {detailSurah.name}
+        </h1>
+      </div>
+
+      <div></div>
+    </div>
+  );
+}
--- src/components/Surah/SurahItem.tsx
+++ src/components/Surah/SurahItem.tsx
@@ -6,6 +6,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import Link from "next/link";
 
 export default function SurahItem({
   query,
@@ -30,27 +31,28 @@ export default function SurahItem({
     );
   };
   return (
-    <Card>
-      <div className="flex text-sm justify-start items-start left-0 top-0 w-auto">
-        <span className="bg-yellow-400 dark:bg-secondary px-2 rounded-t-md rounded-r-md">
-          {number}.
-        </span>
-      </div>
-
-      <CardHeader className="py-0">
-        <CardTitle className="flex font-sans justify-end text-sm py-1">
-          {revelation}
-        </CardTitle>
-        <CardDescription className="flex font-serif mt-0 justify-start font-bold p-0">
-          {highlightQuery(name, query)}
-        </CardDescription>
-      </CardHeader>
-      <CardContent className="py-0">
-        <p>{translation}</p>
-      </CardContent>
-      <CardFooter className="py-1 flex justify-end font-serif">
-        <p>{numberOfAyahs} ayat</p>
-      </CardFooter>
-    </Card>
+    <Link href={`/quran/${number}`}>
+      <Card>
+        <div className="flex text-sm justify-start items-start left-0 top-0 w-auto">
+          <span className="bg-yellow-400 dark:bg-secondary px-2 rounded-t-md rounded-r-md">
+            {number}.
+          </span>
+        </div>
+        <CardHeader className="py-0">
+          <CardTitle className="flex font-sans justify-end text-sm py-1">
+            {revelation}
+          </CardTitle>
+          <CardDescription className="flex font-serif mt-0 justify-start font-bold p-0">
+            {highlightQuery(name, query)}
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="py-0">
+          <p>{translation}</p>
+        </CardContent>
+        <CardFooter className="py-1 flex justify-end font-serif">
+          <p>{numberOfAyahs} ayat</p>
+        </CardFooter>
+      </Card>
+    </Link>
   );
 }
