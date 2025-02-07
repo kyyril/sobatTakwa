a5150cfa937d4a1f3a62fcd0606b444c2da823aa
kyyril <khairilrahman55@gmail.com>
2025-02-07 11:40:02 +0700

    style(surah):card

--- src/components/Surah/SurahList.tsx
+++ src/components/Surah/SurahList.tsx
@@ -9,31 +9,35 @@ interface SurahListProps {
 
 export default function SurahList({ dataSurah }: SurahListProps) {
   const [query, setQuery] = useState("");
-
   return (
-    <div className="w-full flex flex-col gap-4 mt-4 mx-auto">
-      <div className="flex justify-center px-4">
+    <div className="w full flex flex-col gap-y-2 mt-4">
+      <div className="flex justify-center mx-10 lg:mx-48">
         <Input
           onChange={(e) => setQuery(e.target.value.toLowerCase())}
-          placeholder="Cari Surah..."
-          className="max-w-2xl w-full"
+          placeholder="Cari Surah"
         />
       </div>
-      <div className="grid w-full grid-cols-2 gap-1 px-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      <div className="grid w-screen lg:px-2 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {dataSurah
-          .filter((surah) => surah.name.toLowerCase().includes(query))
-          .map((surah) => (
-            <SurahItem
-              key={surah.number}
-              query={query}
-              number={surah.number}
-              name={surah.name}
-              translation={surah.translation}
-              revelation={surah.revelation}
-              numberOfAyahs={surah.numberOfAyahs}
-              description={surah.description}
-            />
-          ))}
+          .filter((surah) => surah.name.toLocaleLowerCase().includes(query))
+          .map(
+            (
+              surah //surah.surah judul surah
+            ) => (
+              <div key={surah.number}>
+                {/* Pass the relevant data to surahItem */}
+                <SurahItem
+                  query={query}
+                  number={surah.number}
+                  name={surah.name}
+                  translation={surah.translation}
+                  revelation={surah.revelation}
+                  numberOfAyahs={surah.numberOfAyahs}
+                  description={surah.description}
+                />
+              </div>
+            )
+          )}
       </div>
     </div>
   );
